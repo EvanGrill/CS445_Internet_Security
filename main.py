49e6f1625b444f271e876540c353989c08d12a0b
Evan Grill <grille@unr.edu>
2019-04-07 14:45:12 -0700

    Initial testing for Chrome

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sqlite3
 from glob import glob
 
 def fetch_os():
+    '''Returns (OS, Version) of the currently running OS.'''
     system = platform.system()
     if system == "Windows":
         return (system, platform.release())
@@ -16,29 +17,66 @@ def fetch_os():
         return ("N/A", "N/A")
 
 def copy_firefox_cookie_db(OS):
-    os.remove("./cookies.sqlite")
+    '''Copies the cookies sqlite database from Firefox from a given
+    OS.  Currently only Mac OS is supported.  Future work will merge
+    copy functions together and take OS and Browser as arguemnts.'''
+    try:
+        os.remove("./ff_cookies.sqlite")
+    except:
+        pass
     if OS[0] == "Mac OS":
         folder = os.path.expanduser("~")+"/Library/Application Support/Firefox/Profiles/"
     else:
         folder = "N/A"
     cookie_location = glob(folder + "*/")[0] + "cookies.sqlite"   
     try:
-        shutil.copyfile(cookie_location, "./cookies.sqlite")
+        shutil.copyfile(cookie_location, "./ff_cookies.sqlite")
         return True
     except:
         return False
 
-def fetch_twitter_token(dbfile):
+def copy_chrome_cookie_db(OS):
+    '''Copies the cookies sqlite database from Chrome from a given
+    OS.  Currently only Mac OS is supported.  Future work will merge
+    copy functions together and take OS and Browser as arguemnts.'''
+    try:
+        os.remove("./ch_cookies.sqlite")
+    except:
+        pass
+    if OS[0] == "Mac OS":
+        folder = os.path.expanduser("~")+"/Library/Application Support/Google/Chrome/Default/"
+    else:
+        folder = "N/A"
+    cookie_location = folder + "Cookies"
+    print(cookie_location)
+    try:
+        shutil.copyfile(cookie_location, "./ch_cookies.sqlite")
+        return True
+    except:
+        return False
+
+
+def fetch_twitter_token(dbfile, browser):
+    '''Retreive auth_token cookie for specific browsers.
+    Presently works for Firefox.  Chrome support is in progress,
+    but only retreives the encrypted cookie. Decryption is not
+    yet available.'''
     conn = sqlite3.connect(dbfile)
     c = conn.cursor()
-    for row in c.execute("SELECT value FROM moz_cookies WHERE baseDomain='twitter.com' AND name='auth_token'"):
+    if browser == "FF":
+        query = "SELECT value FROM moz_cookies WHERE baseDomain='twitter.com' AND name='auth_token'"
+    if browser == "CH":
+        query = "SELECT encrypted_value FROM cookies WHERE host_key='.twitter.com' AND name='auth_token'"
+    for row in c.execute(query):
         return row[0]
     pass
 
 def main():
     OS = fetch_os()
     copy_firefox_cookie_db(OS)
-    print(fetch_twitter_token("./cookies.sqlite"))
+    copy_chrome_cookie_db(OS)
+    print(fetch_twitter_token("./ff_cookies.sqlite", "FF"))
+    print(fetch_twitter_token("./ch_cookies.sqlite", "CH"))
 
 
 if __name__ == "__main__":
